723ff61c04440f9588c1475f421acbf4cb8419bb
Stéphane Bisinger <stephane@sbisinger.ch>
2020-01-29 22:27:56 +0100

    Prefer a Map as the representation of communities

--- src/main/java/ch/aaap/assignment/model/ApplicationModel.java
+++ src/main/java/ch/aaap/assignment/model/ApplicationModel.java
@@ -4,44 +4,53 @@ import ch.aaap.assignment.raw.CSVPoliticalCommunity;
 import ch.aaap.assignment.raw.CSVPostalCommunity;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class ApplicationModel implements Model {
-  Set<CSVPoliticalCommunity> politicalCommunities;
-  Set<CSVPostalCommunity> postalCommunities;
+  Map<String, CSVPoliticalCommunity> politicalCommunities;
+  Map<String, Set<CSVPostalCommunity>> postalCommunities;
 
   public ApplicationModel(
       Set<CSVPoliticalCommunity> politicalCommunities, Set<CSVPostalCommunity> postalCommunities) {
-    this.politicalCommunities = politicalCommunities;
-    this.postalCommunities = postalCommunities;
+    this.politicalCommunities =
+        politicalCommunities.stream()
+            .collect(Collectors.toMap(CSVPoliticalCommunity::getNumber, Function.identity()));
+    this.postalCommunities =
+        postalCommunities.stream()
+            .collect(Collectors.groupingBy(CSVPostalCommunity::getZipCode, Collectors.toSet()));
   }
 
   @Override
   public Set<PoliticalCommunity> getPoliticalCommunities() {
-    return politicalCommunities.stream()
+    return politicalCommunities.values().stream()
         .map(pc -> pc.getPoliticalCommunity())
         .collect(Collectors.toSet());
   }
 
   @Override
   public Set<PostalCommunity> getPostalCommunities() {
-    return postalCommunities.stream()
-        .map(pc -> pc.getPostalCommunity())
+    return postalCommunities.values().stream()
+        .flatMap(pcSet -> pcSet.stream().map(pc -> pc.getPostalCommunity()))
         .collect(Collectors.toSet());
   }
 
   @Override
   public Set<Canton> getCantons() {
-    return politicalCommunities.stream().map(pc -> pc.getCanton()).collect(Collectors.toSet());
+    return politicalCommunities.values().stream()
+        .map(pc -> pc.getCanton())
+        .collect(Collectors.toSet());
   }
 
   @Override
   public Set<District> getDistricts() {
-    return politicalCommunities.stream().map(pc -> pc.getDistrict()).collect(Collectors.toSet());
+    return politicalCommunities.values().stream()
+        .map(pc -> pc.getDistrict())
+        .collect(Collectors.toSet());
   }
 
   public Map<String, Set<PoliticalCommunity>> getPoliticalCommunitiesByCanton() {
-    return this.politicalCommunities.stream()
+    return this.politicalCommunities.values().stream()
         .collect(
             Collectors.groupingBy(
                 CSVPoliticalCommunity::getCantonCode,
@@ -50,7 +59,7 @@ public class ApplicationModel implements Model {
   }
 
   public Map<String, Set<District>> getDistrictsByCanton() {
-    return this.politicalCommunities.stream()
+    return this.politicalCommunities.values().stream()
         .collect(
             Collectors.groupingBy(
                 CSVPoliticalCommunity::getCantonCode,
@@ -58,7 +67,7 @@ public class ApplicationModel implements Model {
   }
 
   public Map<String, Set<PoliticalCommunity>> getPoliticalCommunitiesByDistrict() {
-    return this.politicalCommunities.stream()
+    return this.politicalCommunities.values().stream()
         .collect(
             Collectors.groupingBy(
                 CSVPoliticalCommunity::getDistrictNumber,
